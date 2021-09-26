572e58428a6c6e0172662b86e9ef4783f8c87454
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-26 15:27:23 +0000

    test sums

--- main.sql
+++ main.sql
@@ -4,3 +4,4 @@ The sum of all values in LAT_N rounded to a scale of  2 decimal places.
 The sum of all values in LONG_W rounded to a scale of 2 decimal places.
 */
 
+select sum(lat_n),sum(long_w) from station
